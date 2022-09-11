aff50888f6853b5b1ef3d59cf1dade6d2b5cc53f
Connie Leung <cleung334@gmail.com>
2022-09-11 16:46:30 +0800

    feat: calculate all prime factors

--- src/queue/processors/prime.processor.ts
+++ src/queue/processors/prime.processor.ts
@@ -1,4 +1,20 @@
-import { Processor } from '@nestjs/bull';
+import { Process, Processor } from '@nestjs/bull';
+import { Job } from 'bull';
+import { PrimeService } from '../services';
 
 @Processor('prime')
-export class PrimeProcessor {}
+export class PrimeProcessor {
+  constructor(private primeService: PrimeService) {}
+
+  @Process('prime-factors')
+  findPrimeFactors({ data }: Job<{ input: number }>): void {
+    const primeFactors = this.primeService.getPrimeFactors(data.input);
+    console.log(`${new Date()} - All prime factors`, primeFactors);
+  }
+
+  @Process('distinct-prime-factors')
+  findDistinctPrimeFactors({ data }: Job<{ input: number }>): void {
+    const primeFactors = this.primeService.getPrimeFactors(data.input);
+    console.log(`${new Date()} - All prime factors`, primeFactors);
+  }
+}
--- src/queue/services/prime.service.ts
+++ src/queue/services/prime.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PrimeService {}
+export class PrimeService {
+  getPrimeFactors(num: number): number[] {
+    if (num <= 1) {
+      return [];
+    }
+
+    let copiedNum = num;
+    const primeFactors: number[] = [];
+    let currentPrimeFactor = 2;
+    while (copiedNum % currentPrimeFactor === 0) {
+      primeFactors.push(currentPrimeFactor);
+      copiedNum = copiedNum / currentPrimeFactor;
+    }
+
+    const sqrtRoot = Math.floor(Math.sqrt(num));
+    currentPrimeFactor = 3;
+    while (currentPrimeFactor <= sqrtRoot) {
+      while (copiedNum % currentPrimeFactor === 0) {
+        primeFactors.push(currentPrimeFactor);
+        copiedNum = copiedNum / currentPrimeFactor;
+      }
+      currentPrimeFactor = currentPrimeFactor + 2;
+    }
+
+    if (copiedNum > 2) {
+      primeFactors.push(copiedNum);
+    }
+    return primeFactors;
+  }
+}
